5eaaca983f90f26add0fa74cebe65808968315f4
Mauricio Maciel <macielmauriciio@gmail.com>
2025-04-01 21:10:44 -0300

    implementada a conexão com o banco de dados

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 import express from "express";
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize('bibliotecla2025', 'postgres', 'postgres', {
+    host: 'localhost',
+    port: 5432,
+    dialect: 'postgres',
+    define:{
+        timestamps: false,
+        freezeTableName: true
+    }
+  });
+
+  try {
+    await sequelize.authenticate();
+    console.log('A conexão com o banco foi feita com sucesso.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 
 const app = express();
 app.use(express.json());
@@ -7,6 +25,8 @@ app.get('/teste',(req, res)=> {
     res.send('Teste ok');
 
 });
+
+
 
 
 app.listen(3000, ()=>{console.log('Servidor rodando na porta 3000')})

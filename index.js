57913c58710dee7a84abcdf5276a33126f71f06f
Mauricio Maciel <macielmauriciio@gmail.com>
2025-04-01 22:16:07 -0300

    criado método create

--- index.js
+++ index.js
@@ -64,7 +64,26 @@ app.get('/editora/:id', async (req, res) => {
     res.json(respostaBanco);
     });
 
+app.post('/editora', async (req, res) => {
+    //const nomeeditora = req.body.nomeeditora;
+    //const cnpj = req.body.cnpj;
+    //const endereco = req.body.endereco;
 
+    const respostaBanco = await Editora.create(req.body);
+    res.json(respostaBanco);
+});
 
+app.put('/editora/:id', async (req, res) => {
+    const nomeeditora = req.body.nomeeditora;
+    const cnpj = req.body.cnpj;
+    const endereco = req.body.endereco;
+
+    const ideditora = req.params.id;
+
+    const respostaBanco = await Editora.update(
+        {nomeeditora, cnpj, endereco},
+        {where: {ideditora}});
+    res.json(respostaBanco);
+});
 
 app.listen(3000, ()=>{console.log('Servidor rodando na porta 3000')})

ec121c4a67d3599ac82c615e9909714bc4207c9c
Mauricio Maciel <macielmauriciio@gmail.com>
2025-04-01 21:31:58 -0300

    Mapeamento da model Editora

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { Sequelize } from "sequelize";
+import { Sequelize, DataTypes } from "sequelize";
 
+//configuração da conexão com banco de dados
 const sequelize = new Sequelize('bibliotecla2025', 'postgres', 'postgres', {
     host: 'localhost',
     port: 5432,
@@ -11,11 +12,38 @@ const sequelize = new Sequelize('bibliotecla2025', 'postgres', 'postgres', {
     }
   });
 
+//mapeamento da model Editora
+  const Editora = sequelize.define(
+    'editora',
+    {
+      // Model attributes are defined here
+      ideditora: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      nomeeditora: {
+        type: DataTypes.STRING(60),
+        allowNull: false
+      },
+      cnpj: {
+        type: DataTypes.STRING(20),
+        allowNull: false
+      },
+      endereco: {
+        type: DataTypes.TEXT,
+        allowNull: true
+        },
+    }
+  );
+
+
   try {
     await sequelize.authenticate();
     console.log('A conexão com o banco foi feita com sucesso.');
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.error('Erro ao conectar com o obanco de dados:', error);
   }
 
 const app = express();

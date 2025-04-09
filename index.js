1b180548ccd7b1e30be32b7b177a8fe31f245ffe
Mauricio1maciel <macielmauriciio@gmail.com>
2025-04-08 21:46:46 -0300

    ...

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import banco from "./banco.js";
 import editora from "./controller/EditoraController.js";
+import categoria from "./controller/CategoriaController.js";
+import autor from "./controller/AutorController.js";
 
   try {
     await banco.authenticate();
@@ -22,5 +24,17 @@ app.get('/editora/:id', editora.selecionar);
 app.post('/editora', editora.inserir);
 app.put('/editora/:id', editora.alterar);
 app.delete('/editora/:id', editora.excluir);
+
+app.get('/categoria', categoria.listar);
+app.get('/categoria/:id', categoria.selecionar);
+app.post('/categoria', categoria.inserir);
+app.put('/categoria/:id', categoria.alterar);
+app.delete('/categoria/:id', categoria.excluir);
+
+app.get('/autor', autor.listar);
+app.get('/autor/:id', autor.selecionar);
+app.post('/autor', autor.inserir);
+app.put('/autor/:id', autor.alterar);
+app.delete('/autor/:id', autor.excluir);
 
 app.listen(3000, ()=>{console.log('Servidor rodando na porta 3000')})

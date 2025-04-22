8038f6ad43321a05cb763d98fc528c44818b9449
Mauricio1maciel <macielmauriciio@gmail.com>
2025-04-22 20:23:48 -0300

    questao 1 e 2 Cadastrar funcionário

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import autor from "./controller/AutorController.js";
 import usuario from "./controller/UsuarioController.js";
 import livro from "./controller/LivroController.js";
 import emprestimo from "./controller/EmprestimoController.js";
+import funcionario from "./controller/FuncionarioController.js";
+
 
   try {
     await banco.authenticate();
@@ -18,7 +20,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/teste',(req, res)=> {
-    res.send('Teste ok');
+  res.json('Teste ok');
 });
 
 //rotas crud da tabala editora
@@ -57,6 +59,10 @@ app.get('/emprestimo/:id', emprestimo.selecionar);
 app.post('/emprestar', emprestimo.emprestar);
 app.put('/devolver/:id', emprestimo.devolver);
 
+app.get('/funcionario', funcionario.listar);
+app.get('/funcionario/:id', funcionario.selecionar);
+app.post('/funcionario', funcionario.inserir);
+app.put('/funcionario/:id', funcionario.alterar);
 
 
 app.listen(3000, ()=>{console.log('Servidor rodando na porta 3000')})
